f686f41ca98a984241e4b6cdb971e94cad1a3488
Crozy <contact@paul-leuck.ovh>
2016-11-04 11:01:11 +0100

    Modif ligne 10 plus gros fichier

--- script.sh
+++ script.sh
@@ -10,7 +10,7 @@ echo $(find . -name \*.java | wc -l)
 find . -name \*.java -exec wc -l {} \;
 
 #Lister les 10 plus gros fichiers java
-find . -name \*.java -exec wc -l {} \; | sort -n |tail -10
+find . -name \*.java -exec wc -c {} \; | sort -n |tail -10
 
 #Compter le nombre total de ligne de code java
 nombre=0;
